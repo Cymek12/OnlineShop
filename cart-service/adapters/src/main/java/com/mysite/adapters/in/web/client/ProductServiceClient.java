034e6ff7f0fc31e59384be023d4b0448bff3df18
Cymek12 <jak.cymerman@gmail.com>
2025-05-20 10:47:36 +0200

    open feign

--- cart-service/adapters/src/main/java/com/mysite/adapters/in/web/client/ProductServiceClient.java
+++ cart-service/adapters/src/main/java/com/mysite/adapters/in/web/client/ProductServiceClient.java
@@ -1,5 +1,36 @@
 package com.mysite.adapters.in.web.client;
 
 
-public class ProductServiceClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        value = "app",
+        url = "${spring.cloud.openfeign.client.config.postClient.url}"
+//        configuration = FeignConfig.class,
+//        fallbackFactory = MedicalClinicFallbackFactory.class
+)
+public interface ProductServiceClient {
+//    @GetMapping
+//    ProductDTO getProduct()
 }
+
+
+
+//public interface MedicalClinicClient {
+//
+//    @PatchMapping("/reservation")
+//    VisitDTO reserveVisit(@RequestBody ReserveVisitCommand reserveVisitCommand);
+//
+//    @GetMapping("/available")
+//    PageContent<VisitDTO> getAvailableVisits(
+//            Pageable pageable,
+//            @RequestParam(required = false) String patientEmail,
+//            @RequestParam(required = false) String doctorEmail,
+//            @RequestParam(required = false) String specialization,
+//            @RequestParam(required = false) VisitDayCommand visitDayCommand);
+//}
